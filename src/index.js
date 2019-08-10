36baf4a199849fd7d9fd5b25a554f6d83b75ed8f
Unknown <mosquitojoe85@gmail.com>
2019-08-11 04:00:25 +0800

    [CHANGE] 增加表單

--- src/index.js
+++ src/index.js
@@ -5,12 +5,28 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Payment from './routes/Payment';
 import Success from './routes/Success';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: { main: '#A8D574', contrastText: '#F0F0F0' },
+    divider: { main: '#A8D574' }
+  },
+  typography: {
+    // 告诉Material-UI html元素的字体大小是什么。
+    htmlFontSize: 16,
+  },
+});
 
 ReactDOM.render(
-  <Router>
-    <Route path="/" exact component={Payment} />
-    <Route path="/success" component={Success} />
-  </Router>
+  <ThemeProvider theme={theme}>
+    <Router>
+      <Route path="/" exact component={Payment} />
+      <Route path="/success" component={Success} />
+    </Router>
+  </ThemeProvider>
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- src/routes/Payment/index.js
+++ src/routes/Payment/index.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.scss';
+import logo from '../../assets/logo.png';
+import visa from '../../assets/visa.svg';
+import master from '../../assets/master.svg';
+import jcb from '../../assets/jcb.svg';
+import up from '../../assets/up.svg';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+
+
+const useStyles = makeStyles(theme => ({
+  textField: {
+    paddingTop: theme.spacing(1),
+  },
+  label: {
+    fontSize: 18
+  },
+  input: {
+    fontSize: 24,
+    paddingRight: theme.spacing(1),
+    paddingLeft: theme.spacing(1),
+    color: '#F0F0F0',
+  }
+}));
 
 function Payment() {
+  const classes = useStyles();
+  const [name, setName] = useState('');
+
   return (
     <div className="paymentWrapper">
       <div className="leftCol">
@@ -16,13 +43,56 @@ function Payment() {
             <div className="billLabel billPriceLabel">本筆訂單將支付</div>
             <div className="billPriceValue"><span>1,250</span>元</div>
             <div className="billLabel billPaymethod">支付方式</div>
-            <button className="billPayBtn">信用卡</button>
-            <button className="billPayBtn">網路ATM<span>(晶片讀卡機轉帳)</span></button>
-            <button className="billPayBtn">ATM櫃員機<span>(實體ATM及網銀)</span></button>
+            <div role="button" className="billPayBtn act">信用卡</div>
+            <div role="button" className="billPayBtn">網路ATM<span>(晶片讀卡機轉帳)</span></div>
+            <div role="button" className="billPayBtn">ATM櫃員機<span>(實體ATM及網銀)</span></div>
           </div>
+          <div className="logoBlock"><img src={logo} alt="logo" /></div>
         </div>
       </div>
-      <div className="rightCol">Payment</div>
+      <div className="rightCol">
+        <div className="rightContent">
+          <h1>信用卡付款</h1>
+          <form noValidate autoComplete="off">
+            <div className="creditBlock">
+              <div className="creditType">
+                <button><img src={visa} alt="visa" /></button>
+                <button><img src={master} alt="master" /></button>
+                <button><img src={jcb} alt="jcb" /></button>
+                <button><img src={up} alt="up" /></button>
+              </div>
+              <TextField
+                label="持卡人"
+                className={classes.textField}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                fullWidth
+                required
+                placeholder="請輸入持卡人姓名"
+                InputLabelProps={{ shrink: true, className: classes.label  }}
+                InputProps={{ className: classes.input }}
+                error
+              />
+              <FormControl>
+                <TextField
+                label="持卡人"
+                className={classes.textField}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                fullWidth
+                required
+                placeholder="請輸入持卡人姓名"
+                InputLabelProps={{ shrink: true, className: classes.label  }}
+                InputProps={{ className: classes.input }}
+              />
+              </FormControl>
+
+              <button type="submit">mm</button>
+
+            </div>
+          </form>
+        </div>
+      </div>
 
     </div>
   );
